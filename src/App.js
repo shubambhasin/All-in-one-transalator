eddffa2b9c72741175fbd69079213b6f2188339b
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-01-08 04:47:26 +0530

    fix: squashed few bugs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default function App() {
           Shubam Bhasin
         </a>{" "}
       </footer>
-      <br />
     </div>
   );
 }
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -155,7 +155,7 @@ const Main = () => {
     text_area.value = " ";
   };
   return (
-    <div>
+    <div className="main">
       <h2>All in One Fun-Translator</h2>
       <small>
         *Uses API from{" "}
